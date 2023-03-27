c98994c1b0392157e0623c5723efde1047a37ed3
shakthi <shakthikumar123456789@gmail.com>
2023-03-27 17:51:09 +0530

    Create upload route

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const { File } = require("../models/file");
 const { upload } = require("../middleware/multer");
 
 const router = express.Router();
 
-router.post("/", upload.single("file"), (req, res) => {
-  res.send("hello");
+router.post("/", upload.single("file"), async (req, res) => {
+  const { path, originalname } = req.file;
+  const { password } = req.body;
+
+  const fileData = { path, originalname };
+
+  if (!!password) {
+    fileData.password = await bcrypt.hash(password, 10);
+  }
+
+  const file = await File.create(fileData);
+
+  res.render("index", { fileLink: `${req.headers.origin}/files/${file.id}` });
 });
 
 module.exports = router;
